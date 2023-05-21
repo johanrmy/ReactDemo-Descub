3e8ec5ff40e513622369f37d41674aca82203151
Johan <johan.ramos.m@tecsup.edu.pe>
2023-05-20 23:45:49 -0500

    agregar hooks para ingresar datos desde la API con props

--- src/routes/Muralista.jsx
+++ src/routes/Muralista.jsx
@@ -2,16 +2,34 @@ import ProfileSection from "../components/Profile"
 import ActivitySection from "../components/Activity"
 import DescubNavbar from "../components/Navbar";
 import DescubFooter from "../components/Footer";
-import {Router} from "react-router-dom";
+import React,{useEffect,useState} from "react";
+import { MuralistaApi } from "../data/Muralista";
+import { useParams } from "react-router-dom";
+
 
 function Muralista(){
+  const[muralista,setMuralista] = useState(null)
+  const params = useParams()
+  useEffect(()=>{
+    MuralistaApi(params.id,setMuralista)
+  },[])
     return (
-      <body>
+      <>
         <DescubNavbar />
-        <ProfileSection />
+        {muralista != null ? (
+          <ProfileSection
+            seudonimo={muralista.data.sedonimo}
+            user_instagram={muralista.data.userInstagram}
+            user_facebook={muralista.data.userFacebook}
+            email={muralista.data.email}
+            foto = {muralista.data.foto}
+          />
+        ) : (
+          "CARGANDO"
+        )}
         <ActivitySection />
         <DescubFooter />
-      </body>
+      </>
     );
 }
 
