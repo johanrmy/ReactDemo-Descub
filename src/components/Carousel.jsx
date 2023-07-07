89f1c78ec8a45049427b0a25f9594f99a14bf3cd
ariana2021 <ariana.azabache@tecsup.edu.pe>
2023-07-07 16:44:44 -0500

    vista mural

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,29 +1,54 @@
 import React from 'react'
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-function DescubCarousel(){
-  return(
-
-    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-      <div class="carousel-inner">
-        <div className="carousel-item active" data-bs-interval="50">
-          <img src="./images/rosa.jpg" class="d-block w-50" alt="..."/>
+const DescubCarousel = () => {
+    return (
+  <div class="container text-center">
+    <div class="row align-items-center">
+    <div class="col">
+    <Carousel
+        showArrows={true}
+        showThumbs={false}
+        showStatus={false}
+        infiniteLoop={true}
+        emulateTouch={true}
+        autoPlay={true}
+        interval={900}
+        transitionTime={500}
+        swipeable={true}
+      >
+        <div>
+          <img src="../images/rosario.jpg"  class="col-sm-5 offset-sm-2 col-md-6 offset-md-0" alt="Imagen 1" />
         </div>
-        <div className="carousel-item" data-bs-interval="50">
-          <img src="./images/rosario.jpg" class="d-block w-50" alt="..."/>
+        <div>
+          <img src="../images/rosario.jpg"  class="col-sm-5 offset-sm-2 col-md-6 offset-md-0" alt="Imagen 2" />
         </div>
-        <div className="carousel-item">
-          <img src="./images/rosita.jpg" class="d-block w-50" alt="..."/>
+        <div>
+          <img src="../images/rosita.jpg"  class="col-sm-5 offset-sm-2 col-md-6 offset-md-0" alt="Imagen 3" />
         </div>
-      </div>
-      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Next</span>
-      </button>
+      </Carousel>
     </div>
-  );
-}   
+    <div class="col">
+      <div class = "card" >
+        <div class="card-body"></div>
+        <h5 class="card-title">Detalles del Mural</h5>
+          <p class="card-text">Herencia multicultural
+    “El trabajo de la artista ecuatoriano-estadounidense María “TooFly” Castillo se
+    caracteriza por retratar mujeres empoderadas, luchadoras –cuenta Wagner sobre la elección
+    de los artistas participantes, que han trabajado el mural durante 4 días.
+    </p>
+          <p class="card-text mb-4">
+    “Estoy muy orgullosa de este proyecto, porque ha sido una oportunidad de colaboración
+    entre las mujeres muralistas Shipibo-konibo y Conrad, un estupendo artista urbano de Lima. Ha sido especial trabajar con los peruanos, conocer su cultura
+    </p>
+      
+      </div> 
+    </div>
+  </div>
+</div>
+
+    );
+  };
+
 export default DescubCarousel
--- src/components/Paleta.jsx
+++ src/components/Paleta.jsx
@@ -1,45 +1,64 @@
 import React, { useState } from 'react';
-import { ChromePicker } from 'react-color';
 
-  const DescubPaleta = () => {
+
+const DescubPaleta = () => {
   const [colors, setColors] = useState([
     '#FF0000',
     '#00FF00',
     '#0000FF',
     '#FFFF00',
-    '#00FFFF',
-    '#FF00FF',
+    '#d6249f',
   ]);
+  
 
   const [searchTerm, setSearchTerm] = useState('');
   const [selectedColor, setSelectedColor] = useState('');
 
 
-  const handleColorChange = (color) => {
-    setSelectedColor(color.hex);
-  };
-
   const filteredColors = colors.filter((color) =>
     color.toLowerCase().includes(searchTerm.toLowerCase())
   );
-
+ 
   return (
-    <div>
-         <h1 className="titulo">Paleta de colores</h1>
-      <div className="color-palette">
-        {filteredColors.map((color, index) => (
-          <div
-            key={index}
-            className="color"
-            style={{ backgroundColor: color }}
-            onClick={() => setSelectedColor(color)}
-          ></div>
-        ))}
-      </div>
-      <div className="color-picker">
-        <ChromePicker color={selectedColor} onChange={handleColorChange} />
+
+    <div class="container text-center">
+    <div class="row align-items-center">
+    <div class="col">
+          <div>
+            <h1 className="titulo">Paleta de colores</h1>
+            <div className="color-palette">
+              {filteredColors.map((color, index) => (
+                <div
+                  key={index}
+                  className="color"
+                  style={{ backgroundColor: color }}
+                  onClick={() => setSelectedColor(color)}
+                ></div>         
+                        
+              ))}
+          </div>
+        </div>
+   
+    </div>
+    </div>
+    <div class="row">
+      <div class = "card" >
+        <div class="card-body"></div>
+        <h5 class="card-title">Detalles del Mural</h5>
+          <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing 
+          lit. Est atque maiores, laborum earum distinctio incidunt ullam odio soluta 
+          laudantium numquam ipsum minima, architecto ipsam iure dolorum placeat. Ipsa, 
+          aliquid nihil!</p>
+        <p class="card-text mb-4">Lorem ipsum dolor sit amet consectetur adipisicing 
+        lit. Est atque maiores, laborum earum distinctio incidunt ullam odio soluta 
+        laudantium numquam ipsum minima, architecto ipsam iure dolorum placeat. Ipsa, 
+        aliquid nihil!</p>
+        <div class="container text-center">
+        </div>
+       </div>
       </div>
     </div>
+    
   );
 };
 
